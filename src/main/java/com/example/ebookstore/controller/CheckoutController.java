86ed3317d4484f8b7666d34e5262d56a7a299048
SAGARrAKHE <ghungaresayali@gmail.com>
2022-10-23 19:50:54 +0530

    Rest API integration with fronend

--- src/main/java/com/example/ebookstore/controller/CheckoutController.java
+++ src/main/java/com/example/ebookstore/controller/CheckoutController.java
@@ -2,18 +2,19 @@ package com.example.ebookstore.controller;
 
 import com.example.ebookstore.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
-
 @RestController
-@RequestMapping("/eBookCart")
+@RequestMapping(value = "/eBookCart")
 public class CheckoutController {
 
     @Autowired
     CheckoutService checkoutService;
-        @PostMapping("/getInvoice")
-    public double getInvoice(@RequestBody List<Integer> quantities)
+        @PostMapping(value = "/getInvoice",consumes = MediaType.TEXT_PLAIN_VALUE )
+    public double getInvoice(@RequestBody /*List<Integer> quantities*/ String quantities)  throws ParseException
     {
         return checkoutService.getInvoice(quantities);
     }
--- src/main/java/com/example/ebookstore/service/CheckoutServiceImpl.java
+++ src/main/java/com/example/ebookstore/service/CheckoutServiceImpl.java
@@ -6,9 +6,10 @@ import java.util.List;
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
     @Override
-    public double getInvoice(List<Integer> quantities) {
+    public double getInvoice(/*List<Integer> quantities*/ String quantities) {
 
         //Implement logic to calculate Invoice
+        System.out.println("quantities" + quantities);
         return 1;
     }
 }
